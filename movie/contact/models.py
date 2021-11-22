8e70ca132a517bba44ba2782bd7eabea340517b7
Pungushe <y.borozna81@internet.ru>
2021-11-22 11:40:12 +0300

    MovieSite

---
+++ movie/contact/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Contact(models.Model):
+    """ Подписка на email """
+
+    email = models.EmailField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Контакт"
+        verbose_name_plural = "Контакты"
+
